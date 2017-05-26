bbaa9b0d296e0ccaee279094b449f20f06a53a74
Erik Hellman <iam@erikhellman.com>
2017-05-26 18:00:23 +0200

    Add comments

--- src/hoc/BuildCMSModules.js
+++ src/hoc/BuildCMSModules.js
@@ -1,5 +1,10 @@
 import React from 'react'
 
+// try this:
+// import all components
+// map string data to components
+// return new data object (store?)
+
 export const BuildCMSModules = modules =>
   modules.map(module => {
     const { Component, ...props } = module
